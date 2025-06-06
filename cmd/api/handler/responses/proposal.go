d13fb84ef2af429be7e2537c7e256be70db40c53
mismirnov <mi.smirnov24@gmail.com>
2025-06-06 20:06:11 +0300

    Change swaggertype;

--- cmd/api/handler/responses/proposal.go
+++ cmd/api/handler/responses/proposal.go
@@ -46,7 +46,7 @@ type Proposal struct {
 	NoWithVetoVotingPower string `example:"1000000000" format:"string" json:"no_with_veto_voting_power" swaggertype:"string"`
 	AbstainVotingPower    string `example:"1000000000" format:"string" json:"abstain_voting_power"      swaggertype:"string"`
 
-	Changes  json.RawMessage `json:"changes,omitempty" swaggertype:"object"`
+	Changes  json.RawMessage `format:"json"             json:"changes,omitempty" swaggertype:"string"`
 	Proposer *ShortAddress   `json:"proposer,omitempty"`
 }
 
